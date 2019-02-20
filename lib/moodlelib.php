4943d526ee8bce318c53a8e8094501300e9622c8
Neill Magill <neill.magill@nottingham.ac.uk>
2019-02-20 16:12:53 +0000

    MDL-64427 course: Do not allow dirty cm_info during course delete
    When deleting a course (especially one containing a large amount of
    data) the course mod info cache could contain entries for deleted
    activities for a signifcant amount of time making it possible that
    users could see errors in Moodle.

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -5211,6 +5211,7 @@ function remove_course_contents($courseid, $showfeedback = true, array $options 
                         // Delete cm and its context - orphaned contexts are purged in cron in case of any race condition.
                         context_helper::delete_instance(CONTEXT_MODULE, $cm->id);
                         $DB->delete_records('course_modules', array('id' => $cm->id));
+                        rebuild_course_cache($cm->course, true);
                     }
                 }
             }
@@ -5247,6 +5248,7 @@ function remove_course_contents($courseid, $showfeedback = true, array $options 
         }
         context_helper::delete_instance(CONTEXT_MODULE, $cm->id);
         $DB->delete_records('course_modules', array('id' => $cm->id));
+        rebuild_course_cache($cm->course, true);
     }
 
     if ($showfeedback) {
